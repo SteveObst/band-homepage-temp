ae6a9c6a2f662ee562b9eb110ea4e4ae83542c48
Stefan Obst <stefanobst@Stefans-MacBook-Pro-2.local>
2020-01-30 16:28:28 +0100

    optmized JPG for web with max width of 600px

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src="SoulAffairLogo.jpg" className="logo" alt="logo" />
+        <img src="SoulAffairLogo-Web.jpg" className="logo" alt="logo" />
         <p>
           Coming soon...
         </p>
